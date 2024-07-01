1e0be1d0db65c51d27a32187d3230a57b8c0d3ab
Josetoanto <jarf1369@gmail.com>
2024-07-01 01:33:45 -0600

    Arreglando_Nombres

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const NotFound = () => {
     return(
         <div>
-            <h1>NotFound 404</h1>
+            <h1>No encontrado, error 404</h1>
         </div>
     );
 }
